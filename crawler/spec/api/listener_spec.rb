c559d08b27e9c1061e9068072c5611bb22c3c38b
Alexey Karasev <alex@homa-online.ru>
2014-01-09 07:22:23 +0000

    fixed double behavior in rspec for listener

--- crawler/spec/api/listener_spec.rb
+++ crawler/spec/api/listener_spec.rb
@@ -2,29 +2,17 @@ require "api/listener"
 
 module Api
 
-  class TestScheduler
-    @@pushed=0
-    def self.pushed
-      @@pushed
-    end
-    def async
-      self
-    end
-    def push(args={})
-      @@pushed+=1
-    end
-  end
-
-
   describe "Api::Listener" do
     it "listens on specified port and spawns scheduler on each connection" do
-      scheduler=TestScheduler.new
+      scheduler=double
+      async=double
+      scheduler.stub(:async).and_return(async)
+      async.should_receive(:push).exactly(5).times
       listener=Listener.new host: "localhost", port: 9000, scheduler: scheduler
       listener.async.start
       sleep 0.05
       5.times {TCPSocket.new("localhost", 9000)}
       sleep 0.05
-      TestScheduler.pushed.should==5
     end
     
   end
