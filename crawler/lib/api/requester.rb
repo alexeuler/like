53664b3e73e2ff0b055b99de1397d734166d5401
alex@laptop <karasev.alexey@gmail.com>
2014-01-13 21:32:10 +0400

    brushup

--- crawler/lib/api/requester.rb
+++ crawler/lib/api/requester.rb
@@ -2,15 +2,25 @@ require "net/http"
 require "uri"
 require "celluloid"
 require "json"
+require "timeout"
 module Api
   class Requester
+    
+    VK_TIMEOUT=60
     include Celluloid
+
     def push(args)
       if args[:close]
         args[:socket].close
       else
-        vk_response=Net::HTTP.get_response(URI.parse(args[:request]))
-        response=JSON.parse vk_response.body
+        begin
+          vk_response=Timeout::timeout(VK_TIMEOUT) do
+            Net::HTTP.get_response(URI.parse(args[:request]))
+          end
+          response=JSON.parse vk_response.body
+        rescue Timeout::Error =>e
+          response={error: {error_msg: "Request timeout in #{VK_TIMEOUT} seconds"}}
+        end
         response[:id]=args[:id]
         args[:socket].write response.to_json+"\r\n"
       end
