b32aeceac9b69f9d82089c2eaa4f83b0321e1796
alex@laptop <karasev.alexey@gmail.com>
2014-01-18 00:08:49 +0400

    started vk_api

--- crawler/lib/vk_api.rb
+++ crawler/lib/vk_api.rb
@@ -1,15 +1,45 @@
 require "json"
+require "timeout"
 class VkApi
+
+  TIMEOUT=30
+  RETRIES=3
+
   def initialize(args={})
+    args=defaults.merge args
     @socket=args[:socket]
+    @timeout=args[:timeout]
+    @retries=args[:retries]
   end
   
   def method_missing(method, *args, &block)
+    success=false
+    request=request(method, *args).to_json
+    resp=""
+    while (not success) and @retries > 0
+      begin
+        @socket.puts request
+        resp=Timeout::timeout(@timeout) {@socket.gets}
+        resp["error"] && if resp["error"]["error_msg"]=~/Too many requests/i
+          @retries-=1
+          continue
+        end
+      rescue Exception => e
+        @retries-=1
+      end
+    end
+    JSON.parse resp
+  end
+
+  def request(method, *args)
     method=method.to_s
     method.gsub!("_",".")
-    req={method: method, params: args[0]}
-    @socket.puts req.to_json
-    resp=@socket.gets
-    JSON.parse resp
-  end  
+    {method: method, params: args[0]}
+  end
+
+  private
+
+  def defaults
+    {timeout:TIMEOUT, retires: RETRIES}
+  end
 end
--- crawler/spec/vk_api_spec.rb
+++ crawler/spec/vk_api_spec.rb
@@ -1,7 +1,13 @@
 require "vk_api"
+require "socket"
 describe "VkApi" do
-  it "converts method name with params into hash for api server" do
-    api=VkApi.new
-    api.users_get(uid: "2", v: "3").should == '{"method":"users.get","params":{"uid":"2","v":"3"}}'
+  before :each do
+    @client, @server=Socket.pair(:UNIX, :DGRAM, 0)
+    @api=VkApi.new socket:@client, timeout: 0.1, retries: 3
+  end
+  describe "#request" do
+    it "converts method name with params into hash for api server" do
+      @api.request("users_get", uid: "2", v: "3").should == {method: "users.get", params: {uid: "2",v:"3"}}
+    end
   end
 end
