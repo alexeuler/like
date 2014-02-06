d7ae0fba1243dd13f441493e43f324b5e3edad2a
alleycat@laptop <karasev.alexey@gmail.com>
2014-02-07 01:00:09 +0400

    brushup

--- crawler/lib/crawler/api/scheduler.rb
+++ crawler/lib/crawler/api/scheduler.rb
@@ -25,13 +25,13 @@ module Crawler
         begin
           while incoming=Celluloid.timeout(@timeout) {@socket.gets}
             incoming.chomp!
-            request=make_request incoming
+            request=parse_incoming incoming
             request && self.class.queue.push({socket: @socket, request: request, incoming: incoming})
           end
         rescue Celluloid::Task::TimeoutError
           log.warn "Timeout in scheduler"
         rescue IOError
-          #shutdown is called
+          #shutdown was called
         ensure
           shutdown
         end
@@ -44,26 +44,29 @@ module Crawler
       
       private
 
-      def make_request(request)
+      def parse_incoming(request)
         begin
           hash=JSON.parse(request)
         rescue JSON::ParserError => e
           send_error("Unable to parse request")
           return
         end
+        
         unless hash["method"]
           send_error("Method is not specified")
           return
         end
+        
         res="https://api.vk.com/method/#{hash["method"]}?"
-        if hash["params"]
-          if hash["params"].class.name=="Hash"
-            hash["params"].each_pair {|key,value| res+="#{key.to_s}=#{value.to_s}&"} if hash["params"]
-          else
-            send_error("Params must be a hash")
-            return
-          end
+        return res unless hash["params"]
+        
+        if hash["params"].class.name=="Hash"
+          hash["params"].each_pair {|key,value| res+="#{key.to_s}=#{value.to_s}&"}
+        else
+          send_error("Params must be a hash")
+          return
         end
+        
         res
       end
       
