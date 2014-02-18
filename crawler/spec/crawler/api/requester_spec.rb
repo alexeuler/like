4c3146ee66dd2fc0422805c7871b7ecadb75d741
alleycat@laptop <karasev.alexey@gmail.com>
2014-02-19 01:28:40 +0400

    refactoring

--- crawler/spec/crawler/api/requester_spec.rb
+++ crawler/spec/crawler/api/requester_spec.rb
@@ -22,34 +22,47 @@ module Crawler
           @peer.close
         end
 
-        it "sends http <request> and writes to <socket>: {response: <response>, incoming: <incoming>}\\r\\n" do
-          Net::HTTP.should_receive(:get_response) do |uri|
+        def stub_http_response(args = {})
+          response = args[:response] || {}
+          count = args[:count] || Requester::MAX_RETRIES + 1
+          Net::HTTP.should_receive(:get_response).exactly(count).times do |uri|
             uri.host.should == "vk.com"
             res=double ("response")
-            res.stub(:body).and_return({response: "ok"}.to_json)
+            res.stub(:body).and_return(response.to_json)
+            yield if block_given?
             res
           end
-          requester=Requester.new
-          requester.async.push(request: "http://vk.com", incoming: "incoming", socket: @socket)
+        end
+
+        def make_queue(requester)
+          queue = double("queue")
+          queue.should_receive(:shift).exactly(
+              Requester::MAX_RETRIES).times do |tuple|
+            requester.async.push(tuple)
+          end
+          queue
+        end
+
+        def make_requester(args = {})
+          requester=Requester.new args
+          queue=args[:queue] && make_queue(requester)
+          requester.async.push(request: "http://vk.com", incoming: "incoming",
+                               socket: @socket, queue: queue)
+          requester
+        end
+
+        it "sends http <request> and writes to <socket>: {response: <response>, incoming: <incoming>}\\r\\n" do
+          stub_http_response response: {response: "ok"}, count: 1
+          make_requester
           answer=@peer.gets.chomp
           JSON.parse(answer, symbolize_names: true).should == {response: "ok", incoming: "incoming"}
         end
 
         context "when server doesn't respond" do
           it "retries #{Requester::MAX_RETRIES} times and returns timeout error" do
-            timeout=0.000001
-            requester=Requester.new timeout: timeout
-            Net::HTTP.should_receive(:get_response).exactly(
-                Requester::MAX_RETRIES + 1).times do |uri|
-              raise Celluloid::Task::TimeoutError
-            end
-            queue = double("queue")
-            queue.should_receive(:shift).exactly(
-                Requester::MAX_RETRIES).times do |tuple|
-              requester.async.push(tuple)
-            end
-            requester.async.push(request: "http://vk.com", incoming: "incoming",
-                                 socket: @socket, queue: queue)
+            stub_http_response { raise Celluloid::Task::TimeoutError }
+            timeout = 0.000001
+            make_requester(timeout: timeout, queue: true)
             answer=@peer.gets.chomp
             JSON.parse(answer, symbolize_names: true).should ==
                 {error: {error_msg: "Requester timeout in #{timeout} seconds"}, incoming: "incoming"}
@@ -58,20 +71,8 @@ module Crawler
 
         context "when server responds not with JSON" do
           it "retries #{Requester::MAX_RETRIES} times and returns JSON error" do
-            requester=Requester.new
-            Net::HTTP.should_receive(:get_response).exactly(
-                Requester::MAX_RETRIES + 1).times do |uri|
-              uri.host.should == "vk.com"
-              res=double("response")
-              res.stub(:body).and_return("sdfsdf");
-              res
-            end
-            queue = double("queue")
-            queue.should_receive(:shift).exactly(
-                Requester::MAX_RETRIES).times do |tuple|
-              requester.async.push(tuple)
-            end
-            requester.async.push(request: "http://vk.com", incoming: "incoming", socket: @socket, queue: queue)
+            stub_http_response response: "sdfsdf"
+            make_requester queue: true
             answer=@peer.gets.chomp
             JSON.parse(answer, symbolize_names: true).should == {:error=>{:error_msg=>"Unable to parse json from vk"}, incoming: "incoming"}
           end
@@ -80,18 +81,8 @@ module Crawler
         context "when server responds with {error: ...}" do
           it "retries #{Requester::MAX_RETRIES} times and returns the error" do
             requester=Requester.new
-            Net::HTTP.should_receive(:get_response).exactly(
-                Requester::MAX_RETRIES + 1).times do |uri|
-              uri.host.should == "vk.com"
-              res=double("response")
-              res.stub(:body).and_return({error: "vk error"}.to_json)
-              res
-            end
-            queue = double("queue")
-            queue.should_receive(:shift).exactly(
-                Requester::MAX_RETRIES).times do |tuple|
-              requester.async.push(tuple)
-            end
+            stub_http_response response: {error: "vk error"}
+            queue=make_queue requester
             requester.async.push(request: "http://vk.com", incoming: "incoming", socket: @socket, queue: queue)
             answer=@peer.gets.chomp
             JSON.parse(answer, symbolize_names: true).should == {:error => "vk error", incoming: "incoming"}
