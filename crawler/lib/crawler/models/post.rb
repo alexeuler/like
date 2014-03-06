ba34ffd74528c642be48d10637689eb09f6650a5
alleycat@laptop <karasev.alexey@gmail.com>
2014-03-06 22:57:45 +0400

    added load_or_fetch to post

--- crawler/lib/crawler/models/post.rb
+++ crawler/lib/crawler/models/post.rb
@@ -13,6 +13,19 @@ module Crawler
       has_many :likes
       has_many :likes_user_profiles, through: :likes, source: "user_profile"
 
+      def self.load_or_fetch(id)
+        fetched = Post.fetch(id)
+        existing = Post.where(owner_id: id).to_a
+        existing_ids = existing.map(&:vk_id)
+        fetched.delete_if do |model|
+          existing_ids.include? model.vk_id
+        end
+        fetched.each do |model|
+          model.save
+        end
+        fetched + existing
+      end
+
       def fetch_likes
         user_ids=Like.fetch([vk_id, owner_id]).map(&:user_profile_id)
         users = UserProfile.load_or_fetch(user_ids)
